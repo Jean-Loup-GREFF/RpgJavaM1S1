2dd077b6ca3e9a572f5822d84b1df5bdf139c2f5
jean-loup <jeanloup.greff@gmail.com>
2019-10-23 12:01:45 +0200

    True Stat

--- RPGJava1/src/rpgjava1/Statistic.java
+++ RPGJava1/src/rpgjava1/Statistic.java
@@ -9,5 +9,109 @@
  * @author Jean-Loup
  */
 public class Statistic {
-    private int lp
+    private int hp;
+    private int mp;
+    private int sp;
+    private int strenght;
+    private int vitality;
+    private int dexterity;
+    private int agility;
+    private int intelligence;
+    private int wisdom;
+    private int luck;
+    
+
+    public Statistic(int hp, int mp, int sp, int strenght, int vitality, int dexterity, int agility, int intelligence, int wisdom, int luck) {
+        this.hp = hp;
+        this.mp = mp;
+        this.sp = sp;
+        this.strenght = strenght;
+        this.vitality = vitality;
+        this.dexterity = dexterity;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.luck = luck;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public int getSp() {
+        return sp;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public void setSp(int sp) {
+        this.sp = sp;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public void setVitality(int vitality) {
+        this.vitality = vitality;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+    
 }
