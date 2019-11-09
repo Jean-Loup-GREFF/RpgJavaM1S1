6b0f1a8328c882051d323860e639e08ce2b6088a
jean-loup <jeanloup.greff@gmail.com>
2019-11-09 11:23:48 +0100

    Land improvement and creation of Wall class for disp

--- RPGJava1/src/rpgjava1/Land.java
+++ RPGJava1/src/rpgjava1/Land.java
@@ -3,56 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package rpgjava1;
 
-/**
- *
- * @author Jean-Loup
- */
 import java.util.ArrayList;
 
 public class Land {
     private String name;
-    private ArrayList<Object> ElemMap;
+    private ArrayList<AppOnMap> ElemMap;
     private ArrayList<ArrayList<Integer>> Map;
     
     
-
-    public String getName() {
-        return name;
-    }
-    
-    public void initMap(){
+    public void InitMap(){
         this.name = "";
-        this.ElemMap = new ArrayList<Object>();
-        this.Map = new ArrayList<ArrayList<Integer>>();
+        this.ElemMap = new ArrayList<>();
+        this.Map = new ArrayList<>();
     }
     
-    public void initMap(ArrayList<ArrayList<Object>> Save){
+    public void InitMap(ArrayList<ArrayList<AppOnMap>> Save){
+        
         // Save is an array list with first element name of the map
         // and then the other element is an array of array of all
         // the object in the map
-        int x = 0;
-        int y = 0;
         Integer pos = 0;
-        for (ArrayList<Object> i : Save){
-            ArrayList<Object> line = new ArrayList<Object>(); 
-            for (Object j : i){
+        for (ArrayList<AppOnMap> i : Save){
+            ArrayList<Integer> line; 
+            line = new ArrayList<>();
+            for (AppOnMap j : i){
                 if(this.ElemMap.isEmpty()){
                     pos = 0;
                     line.add(pos);
                 }
-                else if(this.ElemMap.contains(j)){
-                    pos = this.ElemMap.indexOf(j);
-                    line.add(pos);
-                }
-                else{
+                else if(!this.ElemMap.contains(j)){
                     pos = this.ElemMap.size();
                     line.add(pos);
                     this.ElemMap.add(j);
-                     
+                    
+                }
+                else{
+                    pos = this.ElemMap.indexOf(j);
+                    line.add(pos);
                 }
              
             }
+            this.Map.add(line);
             
         }
         
@@ -64,14 +57,21 @@ public class Land {
     
     
     public void displayMap() {
+        
         for (ArrayList<Integer> i : this.Map){
-            ArrayList<Object> line = new ArrayList<Object>(); 
             for (Integer j : i){
-                this.ElemMap(j).getchar();
+                this.ElemMap.get(j).getDisplay();
             }
             
         }
     }
     
     
+    public String getName() {
+        return name;
+    }
+    
+    public void moveTo (Integer x, Integer y, Integer Bouton){
+        
+    }
 }
---
+++ RPGJava1/src/rpgjava1/Wall.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpgjava1;
+
+/**
+ *
+ * @author Jean-Loup
+ */
+public class Wall extends AppOnMap{
+    private char wall;
+
+    public Wall() {
+        this.wall = '#';
+    }
+
+    
+    
+}
